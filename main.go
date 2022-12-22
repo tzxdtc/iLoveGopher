0f2bd162345d564b71865a6d057ff0895a9d4716
Zhixiang Tan <tzxdtc10@gmail.com>
2022-12-22 10:53:42 +0900

    test

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type album struct {
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "tannnnn", Price: 39.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
